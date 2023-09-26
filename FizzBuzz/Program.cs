492f9a6b8946b4bf0d1ef6ad55b884acd58d5f59
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-09-26 03:27:41 +0100

    FizzBuzz Defi made in C#

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int counter = 1; counter <= 100; counter++)
+{
+    if((counter % 3 == 0) && (counter % 5 == 0))
+        Console.WriteLine($"{counter} - FizzBuzz");
+    else if (counter % 3 == 0)
+        Console.WriteLine($"{counter} - Fizz");
+    else if (counter % 5 == 0)
+        Console.WriteLine($"{counter} - Buzz");
+    else
+        Console.WriteLine(counter);
+}
