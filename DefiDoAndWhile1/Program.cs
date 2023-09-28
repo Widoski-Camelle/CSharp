3c66ed6d9c15623e910f210975f7f47c513542a5
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-09-28 18:03:09 +0100

    Handle user input of integer type in C#

---
+++ DefiDoAndWhile1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter an integer value between 5 and 10");
+bool validEntry = false;
+int verifyNumber = 0;
+
+while (validEntry == false)
+{
+    string? number = Console.ReadLine();
+    validEntry = int.TryParse(number, out verifyNumber);
+
+    if (validEntry)
+    {
+        if (verifyNumber >= 5 && verifyNumber <= 10 )
+            break;
+
+        else
+            Console.WriteLine($"You entered {verifyNumber}. Please enter a number between 5 and 10.");
+            validEntry = false;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+}
+
+Console.WriteLine($"Your input value ({verifyNumber}) has been accepted.");
