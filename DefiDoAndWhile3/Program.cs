8afd372d0dd21352f483e5cd197e92a127e82a5b
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-09-29 03:10:06 +0100

    Handle user input in array in C#

---
+++ DefiDoAndWhile3/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+int periodLocation = 0;
+
+foreach (string myString in myStrings)
+{
+    periodLocation = myString.IndexOf(".");
+    
+    string mySentence;
+
+    string newString = myString;
+    
+    while (periodLocation != -1)
+    {
+        mySentence = newString.Remove(periodLocation);
+
+        newString = newString.Substring(periodLocation + 1).TrimStart();
+
+        periodLocation = newString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+ 
+    mySentence = newString.Trim();
+    Console.WriteLine(mySentence);
+}
