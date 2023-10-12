7ba92eef20d47ff8350d4368e8eb24b7b9ca9d26
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-10-12 13:07:13 +0100

    Fight Game made in C#

---
+++ HeroVsMonster/Program.cs
@@ -0,0 +1,24 @@
+﻿Random blows = new Random();
+
+int heroHealth = 10;
+int monsterHealth = 10;
+int attack = 0;
+
+do
+{
+    if (heroHealth > 0)
+    {
+        attack = blows.Next(1, 10);
+        monsterHealth -= attack;
+        Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monsterHealth} health.");
+    }
+
+    if (monsterHealth > 0)
+    {
+        attack = blows.Next(1, 10);
+        heroHealth -= attack;
+        Console.WriteLine($"Hero was damaged and lost {attack} health and now has {heroHealth} health.");
+    }
+} while ((heroHealth > 0) && (monsterHealth > 0));
+
+Console.WriteLine(heroHealth > monsterHealth ? "Hero wins" : "Monster wins");
