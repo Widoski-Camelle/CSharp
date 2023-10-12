b9938d6f1f01c9088fe76ff2adf5daea4b3944ff
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-10-12 13:04:51 +0100

    Manage string user input

---
+++ DefiDoAndWhile2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+bool validEntry = false;
+string roleChecked = "";
+
+while (validEntry == false)
+{
+    string? role = Console.ReadLine();
+    roleChecked = role.Trim().ToLower();
+
+    if ((roleChecked == "administrator") || (roleChecked == "manager") || (roleChecked == "user"))
+        validEntry = true;
+        
+    else
+        Console.WriteLine($"The role name that you entered, \"{roleChecked}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+}
+
+Console.WriteLine($"Your input value ({roleChecked}) has been accepted.");
