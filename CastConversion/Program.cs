8673cf687dc342026be826aaeaa956f2342bc4ea
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-12-19 15:10:13 +0100

    Defi of cast and conversion in C#

---
+++ CastConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (string value in values)
+{
+    decimal result;
+    
+    if (decimal.TryParse(value, out result))
+    {
+        total += result;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}\nTotal: {total}");
