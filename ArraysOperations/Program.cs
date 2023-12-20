2ebd6cabeaec4b6a69fb2048273cb48863a13902
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-12-20 14:44:01 +0100

    Learning and Usage of many Array and String methods

---
+++ ArraysOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Funtions to use : Split(), ToCharArray(), Array.Reverse() and String.Join()
+// Result waited   : ehT kciuq nworb xof spmuj revo eht yzal god
+
+string[] items = pangram.Split(' ');
+
+List<string> reversedWords = new List<string>();
+
+foreach (string item in items)
+{
+    char[] everyWord = item.ToCharArray();
+    Array.Reverse(everyWord);
+    reversedWords.Add(new string(everyWord));
+}
+
+string pangramReversed = String.Join(' ', reversedWords);
+Console.WriteLine(pangramReversed);
