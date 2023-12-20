bfd93791ee0ad32b1abc089421d24d59008d69da
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2023-12-20 14:46:06 +0100

    Defi: analyzing and treatment of data of logistics enterprise

---
+++ ArraysOperations2/Program.cs
@@ -0,0 +1,16 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] ordersID = orderStream.Split(',');
+Array.Sort(ordersID);
+
+foreach(string orderID in ordersID)
+{
+    if(orderID.Length == 4)
+    {
+        Console.WriteLine(orderID);
+    }
+    else
+    {
+        Console.WriteLine($"{orderID}\t - Error");
+    }
+}
