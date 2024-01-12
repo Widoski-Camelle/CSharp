48ed443e95acc0dfe8ac22dee873502505514811
Camelle-Widoski <camelle-widoski.houndagba@epitech.eu>
2024-01-12 16:03:06 +0100

    Methods Remove and Replace in C#

---
+++ AssistanceMethodsPartThree/Program.cs
@@ -0,0 +1,10 @@
+﻿// Method Remove
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+
+// Method Replace
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
