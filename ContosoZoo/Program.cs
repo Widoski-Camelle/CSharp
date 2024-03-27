9225c41293b605ea840cb502d7063ffa2ae93dba
Widoski-Camelle <camelle-widoski.houndagba@epitech.eu>
2024-03-27 12:34:19 +0100

    Contoso's Zoo [C# App] : randomize animals

---
+++ ContosoZoo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+RandomizeAnimals();
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
+
+// string[,] group = AssignGroup();
+
+Console.WriteLine("School A");
+
+// PrintGroup(group);
